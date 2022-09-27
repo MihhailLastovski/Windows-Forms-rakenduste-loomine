9f1c81af02f153555ec9b15d4e9b85b604c22889
Mihhail Lastovski <lastovskim@gmail.com>
2022-09-28 01:02:49 +0300

    upd

--- Windows Forms rakenduste loomine/Form1.cs
+++ Windows Forms rakenduste loomine/Form1.cs
@@ -15,7 +15,13 @@ namespace Windows_Forms_rakenduste_loomine
         public ImageForm()
         {
             Text = "Picture Viewer";
-            Size = new Size(400, 400);
+            ClientSize = new Size(529, 330); 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            colorDialog1 = new ColorDialog();
+            openFileDialog1 = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            checkBox1 = new CheckBox();
             TableLayoutPanel tableLayoutPanel = new TableLayoutPanel 
             {
                 ColumnCount = 2,
@@ -25,16 +31,13 @@ namespace Windows_Forms_rakenduste_loomine
                 Size = new Size(529, 330),
                 TabIndex = 0,
             };
-            this.tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
-            this.tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
-            this.tableLayoutPanel.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.checkBox1, 0, 1);
-            this.tableLayoutPanel.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
-            this.tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
-            this.tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            this.tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            PictureBox pictureBox1 = new PictureBox 
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 85F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            pictureBox1 = new PictureBox 
             {
                 BorderStyle = BorderStyle.Fixed3D,
                 Dock = DockStyle.Fill,
@@ -43,8 +46,8 @@ namespace Windows_Forms_rakenduste_loomine
                 TabIndex = 0,
                 TabStop = false
         };
-            this.tableLayoutPanel.SetColumnSpan(this.pictureBox1, 2);
-            CheckBox checkBox1 = new CheckBox 
+            tableLayoutPanel.SetColumnSpan(pictureBox1, 2);
+            checkBox1 = new CheckBox 
             {
                 AutoSize = true,
                 Location = new Point(3, 300),
@@ -54,8 +57,82 @@ namespace Windows_Forms_rakenduste_loomine
                 UseVisualStyleBackColor = true,
             };
             checkBox1.CheckedChanged += new EventHandler(this.checkBox1_CheckedChanged);
-            InitializeComponent();
+            FlowLayoutPanel flowLayoutPanel1 = new FlowLayoutPanel 
+            {
+                AutoSize = true,
+                Dock = DockStyle.Fill,
+                FlowDirection = FlowDirection.RightToLeft,
+                Location = new Point(82, 300),
+                Size = new Size(444, 27),
+                TabIndex = 2
+            };
             
+            Button showButton = new Button 
+            {
+                AutoSize = true,
+                Location = new Point(353, 3),
+                Name = "showButton",
+                Size = new Size(88, 23),
+                TabIndex = 0,
+                Text = "Show a picture",
+                UseVisualStyleBackColor = true
+            };
+            showButton.Click += new EventHandler(ShowButton_Click);
+            Button clearButton = new Button 
+            {
+                AutoSize = true,
+                Location = new Point(253, 3),
+                Name = "clearButton",
+                Size = new Size(94, 23),
+                TabIndex = 1,
+                Text = "Clear the picture",
+                UseVisualStyleBackColor = true
+            };
+            clearButton.Click += new System.EventHandler(clearButton_Click);
+            Button backgroundButton = new Button 
+            {
+                AutoSize = true,
+                Location = new Point(110, 3),
+                Name = "backgroundButton",
+                Size = new Size(137, 23),
+                TabIndex = 2,
+                Text = "Set the background color",
+                UseVisualStyleBackColor = true
+            };
+            backgroundButton.Click += new System.EventHandler(backgroundButton_Click);
+            Button closeButton = new Button
+            {
+                AutoSize = true,
+                Location = new Point(29, 3),
+                Name = "closeButton",
+                Size = new Size(75, 23),
+                TabIndex = 3,
+                Text = "Close",
+                UseVisualStyleBackColor = true
+            };
+            closeButton.Click += new System.EventHandler(closeButton_Click);
+            openFileDialog1 = new OpenFileDialog 
+            {
+                Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" + "s (*.*)|*.*",
+                Title = "Select a picture file"
+            };
+            Controls.Add(tableLayoutPanel);
+            tableLayoutPanel.ResumeLayout(false);
+            tableLayoutPanel.PerformLayout();
+            ((ISupportInitialize)(pictureBox1)).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            tableLayoutPanel.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel.Controls.Add(checkBox1, 0, 1);
+            tableLayoutPanel.Controls.Add(flowLayoutPanel1, 1, 1);
+            flowLayoutPanel1.Controls.Add(showButton);
+            flowLayoutPanel1.Controls.Add(clearButton);
+            flowLayoutPanel1.Controls.Add(backgroundButton);
+            flowLayoutPanel1.Controls.Add(closeButton);
+            
+
+
         }
         private void ShowButton_Click(object sender, EventArgs e)
         {
@@ -80,7 +157,7 @@ namespace Windows_Forms_rakenduste_loomine
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            Close();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)

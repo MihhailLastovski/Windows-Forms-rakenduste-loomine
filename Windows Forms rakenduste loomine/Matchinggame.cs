cef1648844eaf52a7672a549f34cb87693e36bfe
Mihhail Lastovski <lastovskim@gmail.com>
2022-10-09 22:45:55 +0300

    translate

--- Windows Forms rakenduste loomine/Matchinggame.cs
+++ Windows Forms rakenduste loomine/Matchinggame.cs
@@ -10,7 +10,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-
 namespace Windows_Forms_rakenduste_loomine
 {
     public partial class Matching_game : Form
@@ -19,20 +18,19 @@ namespace Windows_Forms_rakenduste_loomine
         TableLayoutPanel tableLayoutPanel;
         Label firstClicked = null;
         Label secondClicked = null;
+        //Taimerite ja nende intervallide loomine
         Timer timer1 = new Timer { Interval = 750 };
         Timer timer = new Timer { Interval = 1000 };
         int score = 0;
         int tik = 0;
         Label difficult;
-
         public Matching_game()
         {
-            CenterToScreen();
-            
+            CenterToScreen(); //Tsentreerib vormi  
             Text = "Matching game";
             ClientSize = new Size(550, 550);
             BackColor = Color.Salmon;
-            difficult = new Label 
+            difficult = new Label //Sildi loomine
             {
                 Text = "Valige raskusaste",
                 Location = new Point(110,100),
@@ -40,9 +38,9 @@ namespace Windows_Forms_rakenduste_loomine
                 Font = new Font("Arial", 28, FontStyle.Bold)
             };
             this.Controls.Add(difficult);
-            string[] buttonstext = { "Lihtne", "Tavaline", "Raske" };
+            string[] buttonstext = { "Lihtne", "Tavaline", "Raske" }; //Massiiv nupul oleva tekstiga
             int y = 200;
-            for (int i = 0; i < buttonstext.Length; i++)
+            for (int i = 0; i < buttonstext.Length; i++) //Nuppude loomine
             {
                 
                 Button button = new Button 
@@ -51,18 +49,19 @@ namespace Windows_Forms_rakenduste_loomine
                     Location = new Point(210,y),
                     Size = new Size(100,80)
                 };
-                button.Click += Button_Click;
+                button.Click += Button_Click; //Nuppude lisamise meetod
                 this.Controls.Add(button);
                 y += 100;
             }
             
             
         }
-        public Matching_game(int x, int y, List<string> icons, TableLayoutPanel tableLayoutPanel) 
+        public Matching_game(int x, int y, List<string> icons, TableLayoutPanel tableLayoutPanel) //Mänguklassi loomine
         {
+            //Taimerite meetodi lisamine
             timer.Tick += Timer_Tick;
             timer1.Tick += timer1_Tick;
-            for (int i = 0; i < x; i++)
+            for (int i = 0; i < x; i++) //Sildi loomine
             {
                 tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
                 tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
@@ -83,14 +82,14 @@ namespace Windows_Forms_rakenduste_loomine
                 };
 
             }
-            foreach (Control control in tableLayoutPanel.Controls)
+            foreach (Control control in tableLayoutPanel.Controls) //Sirvib läbi kõik tabelisLayoutPanel olevad komponendid
             {
-                Label iconLabel = control as Label;
-                if (iconLabel != null)
+                Label iconLabel = control as Label; 
+                if (iconLabel != null) //Kui silt on olemas, siis
                 {
-                    int randomNumber = rnd.Next(icons.Count);
-                    iconLabel.Text = icons[randomNumber];
-                    icons.RemoveAt(randomNumber);
+                    int randomNumber = rnd.Next(icons.Count); //Genereerib juhusliku arvu
+                    iconLabel.Text = icons[randomNumber]; //Võrdleb sildi teksti indeksi järgi massiivi väärtusega
+                    icons.RemoveAt(randomNumber); //Eemaldab selle väärtuse massiivist
                 }
                 iconLabel.ForeColor = iconLabel.BackColor;
                 iconLabel.Click += label1_Click;
@@ -104,14 +103,14 @@ namespace Windows_Forms_rakenduste_loomine
             void label1_Click(object sender, EventArgs e)
             {
                 timer.Start();
-                if (timer1.Enabled == true)
+                if (timer1.Enabled == true) //Kontrollib, kas taimer on käivitatud, kontrollides atribuudi Enabled väärtust.
                     return;
 
                 Label clickedLabel = sender as Label;
 
-                if (clickedLabel != null)
+                if (clickedLabel != null) //Kui mängija valib esimese ja teise sildielemendi ning taimer käivitub
                 {
-                    if (clickedLabel.ForeColor == Color.Black)
+                    if (clickedLabel.ForeColor == Color.Black) //Kui klõpsate juba nähtaval sildil, siis ei juhtu midagi
                         return;
 
                     if (firstClicked == null)
@@ -121,20 +120,21 @@ namespace Windows_Forms_rakenduste_loomine
                         return;
                     }
 
-                    secondClicked = clickedLabel;
+                    secondClicked = clickedLabel; //Jälgib teist klõpsu ja määrab sildi mustaks
                     secondClicked.ForeColor = Color.Black;
-                    timer1.Start();
+                    timer1.Start(); //Käivitab taimeri
                 }
             }
 
             void timer1_Tick(object sender, EventArgs e)
             {
-                if (firstClicked.Text == secondClicked.Text)
+                //Kontrollib iga taimeri linnukest
+                if (firstClicked.Text == secondClicked.Text) //Kui see sobib, muudab see sildi värvi mustaks
                 {
                     firstClicked.ForeColor = firstClicked.ForeColor;
                     secondClicked.ForeColor = secondClicked.ForeColor;
                 }
-                else
+                else //Muidu peidab
                 {
                     firstClicked.ForeColor = firstClicked.BackColor;
                     secondClicked.ForeColor = secondClicked.BackColor;
@@ -142,14 +142,15 @@ namespace Windows_Forms_rakenduste_loomine
                 }
                 firstClicked = null;
                 secondClicked = null;
-                timer1.Stop();
-                CheckForWinner();
+                timer1.Stop(); //Peatab taimeri
+                CheckForWinner(); //Käivitab funktsiooni
             }
 
             void CheckForWinner()
             {
-                foreach (Control control in tableLayoutPanel.Controls)
+                foreach (Control control in tableLayoutPanel.Controls) //Sirvib läbi kõik tabelisLayoutPanel olevad komponendid
                 {
+                    //Kui silmus läbib kõik oksad ja ei naase, siis on mäng läbi
                     Label iconLabel = control as Label;
 
                     if (iconLabel != null)
@@ -158,13 +159,28 @@ namespace Windows_Forms_rakenduste_loomine
                             return;
                     }
                 }
-                timer.Stop();
-                FailedScoreTofile(score);
-                MessageBox.Show("Sa sobitasid kõik ikoonid!", "Palju õnne");
-                MessageBox.Show($"Vead: {score.ToString()}\nAeg sekundid: {tik.ToString()}!", "Tulemus!");
+                timer.Stop(); //Peatab taimeri
+                FailedScoreTofile(score); //Käivitab funktsiooni
+                MessageBox.Show("Sa sobitasid kõik ikoonid!", "Palju õnne"); //Kuvab teate mängu lõppemise kohta
+                restarGame(); //Peatab taimeri
+            }
+            void restarGame() //Taaskäivitab vormi sõltuvalt vastusest
+            {
+                if (MessageBox.Show($"Vead: {score.ToString()}\nAeg sekundid: {tik.ToString()}!\nKas soovite uuesti mängida?", "Tulemus!", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+
+                    Application.Restart();
+                    Environment.Exit(0);
+                }
+                else 
+                {
+                    Application.Exit();
+                    
+                }
+
             }
 
-            void FailedScoreTofile(int score)
+            void FailedScoreTofile(int score) //Meetod kirjutab faili halbade vastete arvu ja kulunud aja
             {
                 StreamWriter to_file = new StreamWriter(@"..\..\..\Score.txt", true);
 
@@ -172,6 +188,7 @@ namespace Windows_Forms_rakenduste_loomine
                 to_file.Close();
             }
         }
+        //Ikoonidega loendid
         List<string> icons = new List<string>()
         {
             "!", "!", "N", "N", "r", "r",
@@ -187,10 +204,10 @@ namespace Windows_Forms_rakenduste_loomine
             "!", "!", "N", "N", ",", ",", "k", "k", "`", "`",
             "b", "b", "v", "v", "w", "w", "z", "z", "f", "f"
         };
-        private void Button_Click(object sender, EventArgs e)
+        private void Button_Click(object sender, EventArgs e) //Meetod raskete mängude valimiseks
         {
             Button nupp_sender = (Button)sender;
-            this.Controls.Clear();
+            this.Controls.Clear(); //Tühjendab vormi
             tableLayoutPanel = new TableLayoutPanel
             {
                 BackColor = Color.MediumVioletRed,
@@ -198,6 +215,7 @@ namespace Windows_Forms_rakenduste_loomine
                 CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset,
             };
             Controls.Add(tableLayoutPanel);
+            //Kontrollib, millist nuppu vajutati
             if (nupp_sender.Text == "Lihtne")
             {
                 
@@ -216,5 +234,6 @@ namespace Windows_Forms_rakenduste_loomine
                 new Matching_game(5, 4, icons_3, tableLayoutPanel);
             }        
         }
+        
     }
 }
